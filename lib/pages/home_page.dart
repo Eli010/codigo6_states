638b5364bea42f14af66339d44a6354bf99a2467
Elí Tp <eli.tp.system@example.com>
2023-04-20 19:12:39 -0500

    provider falta finalizar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,17 +5,27 @@ import 'package:codigo6_states/providers/post_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //MEJORAREMOS NUESTRO CODIGO CON PROVIDER MOSTRANDO DOS PANTALLA INFO
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      PostProvider postProvider =
+          Provider.of<PostProvider>(context, listen: false);
+      postProvider.getPost2();
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    //PRROVIDER DE POSTPROVIDER
-    //recibimos información para eso cambiaremos listen= false
-    PostProvider postProvider =
-        Provider.of<PostProvider>(context, listen: false);
-    //obtiene informacion y notificamos
-    postProvider.getPost2();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.indigo,
@@ -31,10 +41,9 @@ class HomePage extends StatelessWidget {
         },
         child: const Icon(Icons.add),
       ),
-      //USAREMOS NUESTRO APISERVICE- Y PROVIDER
       body: Consumer<PostProvider>(
-        builder: (context, value, _) {
-          return Text(postProvider.posts.toString());
+        builder: (context, provider, _) {
+          return Text(provider.posts.toString());
         },
       ),
     );
